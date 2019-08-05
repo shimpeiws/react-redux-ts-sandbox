0cff0a3baa5b59d185ef7a229344d6d057e339a3
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-08-05 16:52:03 +0900

    Remove history params

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,15 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import store from './store';
 import { Provider } from 'react-redux';
-import createBrowserHistory from 'history/createBrowserHistory';
 import { Routes } from './Routes';
 import { HashRouter } from 'react-router-dom';
 
-const history = createBrowserHistory();
-
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter history={history}>
+    <HashRouter>
       <Routes />
     </HashRouter>
   </Provider>,
