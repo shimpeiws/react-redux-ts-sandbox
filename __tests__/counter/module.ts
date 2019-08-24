68e4d8802562c598c9fd3ece9322717c25bd328b
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-08-25 04:40:30 +0900

    Fix for eslint

--- __tests__/counter/module.ts
+++ __tests__/counter/module.ts
@@ -1,13 +1,17 @@
-import reducer, { initialState, ActionNames } from '../../src/counter/module';
+import reducer, { initialState, ActionNames } from "../../src/counter/module";
 
-test('ActionNames.INC', () => {
-  expect(reducer(initialState, { type: ActionNames.INC, plusAmount: 10 })).toMatchObject({
+test("ActionNames.INC", () => {
+  expect(
+    reducer(initialState, { type: ActionNames.INC, plusAmount: 10 })
+  ).toMatchObject({
     num: 10
   });
 });
 
-test('ActionNames.DEC', () => {
-  expect(reducer(initialState, { type: ActionNames.DEC, minusAmount: 10 })).toMatchObject({
+test("ActionNames.DEC", () => {
+  expect(
+    reducer(initialState, { type: ActionNames.DEC, minusAmount: 10 })
+  ).toMatchObject({
     num: -10
   });
 });
