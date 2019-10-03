061754ffa02ce55cc6edc04f32d80a2fb7e0e307
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-10-03 15:16:54 +0900

    Introduce Auth0

--- src/Auth0/Signup/Signup.tsx
+++ src/Auth0/Signup/Signup.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+import createAuth0Client from "@auth0/auth0-spa-js";
 
 export interface Props {}
 
 export const Signup: React.FC<Props> = props => {
+  console.info("createAuth0Client", createAuth0Client);
   return (
     <div>
       <h2>Signup</h2>
