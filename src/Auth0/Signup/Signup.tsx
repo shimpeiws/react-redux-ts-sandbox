d0fd56d5032c1f1462b3537718a1972dcd6739da
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-10-03 14:54:25 +0900

    Add signup component

---
+++ src/Auth0/Signup/Signup.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface Props {}
+
+export const Signup: React.FC<Props> = props => {
+  return (
+    <div>
+      <h2>Signup</h2>
+    </div>
+  );
+};
