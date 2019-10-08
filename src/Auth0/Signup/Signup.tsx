60daf36564f1f8c6141ff24d9dcaa5f6ae7c2790
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-10-08 14:25:22 +0900

    Hnadle signin

--- src/Auth0/Signup/Signup.tsx
+++ src/Auth0/Signup/Signup.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { SignupState } from "./Module";
 import { ActionDispatcher } from "./Container";
+import { client as Auth0Client } from "../../lib/Auth0";
 
 export interface Props {
   value: SignupState;
@@ -8,15 +9,38 @@ export interface Props {
 }
 
 export const Signup: React.FC<Props> = props => {
+  const [signin, setSignin] = React.useState(false);
+
+  React.useEffect(() => {
+    const init = async () => {
+      const r = await isSginIn();
+      setSignin(r);
+    };
+    init();
+  }, []);
+
   const handleSignup = () => {
     props.actions.signup();
+  };
+
+  const isSginIn = async (): Promise<boolean> => {
+    const client = await Auth0Client();
+    const user = client.getUser();
+    return !!user;
+  };
+
+  const showSignin = () => {
+    if (signin) {
+      return <p>Logout</p>;
+    }
+    return <button onClick={() => handleSignup()}>signup</button>;
   };
 
   return (
     <div>
       <h2>Signup</h2>
       {props.value.loading}
-      <button onClick={() => handleSignup()}>signup</button>
+      {showSignin()}
     </div>
   );
 };
